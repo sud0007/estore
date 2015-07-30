333b489b25b35db1b0333596ab3daee78202ac67
user1 <user1@ubuntu.com>
2015-07-31 00:57:17 +0530

    First html web server repo without kancart

---
+++ html/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.0.104/admin/');
+define('HTTP_CATALOG', 'http://192.168.0.104/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.0.104/admin/');
+define('HTTPS_CATALOG', 'http://192.168.0.104/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'user.123');
+define('DB_DATABASE', 'MYDB');
+define('DB_PREFIX', 'oc_');
---
+++ html/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.0.104/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.0.104/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'user.123');
+define('DB_DATABASE', 'MYDB');
+define('DB_PREFIX', 'oc_');
